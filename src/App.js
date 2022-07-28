8aedf6493e4f133aeee04d42668d5725b422783e
Екатерина Гривнак <hryvnak@flarian.ru>
2022-07-28 19:37:00 +0300

    prepare App.js and index.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button/Button';
+
+const showAlert = () => alert('Шо по русні');
+const showAlert2 = () => alert('Русні пизда');
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button onClick={ showAlert } type="button" autofocus  variant="contained" size="small" buttonText="Button" />
     </div>
   );
 }
