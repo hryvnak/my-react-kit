38355439996918ead5e83438a0066025318c8eee
Екатерина Гривнак <hryvnak@flarian.ru>
2022-08-05 13:22:59 +0300

    add Button component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,27 +1,63 @@
 import cn from 'classnames';
-import { upperFirst } from 'lodash';
+import './Button.scss';
+import { has } from 'lodash';
 
 const Button = (props) => {
-  const { 
-    variant = "contained", 
-    color = "basic", 
-    size = "normal", 
-    buttonText, 
-    onClick, 
-    ...atributs 
-  } = props;
+	const { 
+		children,
+		variant = "contained", 
+		color = "primary", 
+		size = "medium", 
+		text, 
+		onClick,
+		startIcon,
+		endIcon,
+		...atributs 
+	} = props;
 
-  const classeNames = [ variant, color, size ];
-  const normalizedClassNames = classeNames.map(upperFirst);
-  const buttonClass = cn(normalizedClassNames);
+	const classeNames = [ variant, color, size ];
 
-  return (
-    <button 
-      onClick={ onClick } 
-      className={ buttonClass } 
-      { ...atributs }>
-      { buttonText }
-    </button>
-  )
+	let buttonClass = cn("button", classeNames);
+
+	if (has(atributs, 'href')) {
+		return (
+		<a onClick={ onClick } className={ buttonClass } { ...atributs }>
+			{ text }
+		</a>
+		)
+	}
+	else if (has(atributs, 'component')) {
+		if (atributs.component === "label") {
+			return (
+				<label onClick={ onClick } className={ buttonClass } { ...atributs }>
+					{ text }
+					{ children }
+				</label>
+			)
+		}
+	}
+	else if (startIcon) {
+		return (
+			<button onClick={ onClick } className={ buttonClass} { ...atributs }>
+				<span className={ cn("icon", "start", startIcon) }></span>
+				{ text }
+			</button>
+		)
+	}
+	else if (endIcon) {
+		return (
+			<button onClick={ onClick } className={ buttonClass} { ...atributs }>
+				{ text }
+				<span className={ cn("icon", "end", endIcon) }></span>
+			</button>
+		)
+	}
+	else {
+		return (
+		<button onClick={ onClick } className={ buttonClass } { ...atributs }>
+			{ text }
+		</button>
+		)  
+	}
 }
 export default Button;
